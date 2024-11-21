33ea6d6d69cb86f486a1cbe1a5dc6e948bf2bb3d
lucas cunha <lucasgalvao260@gmail.com>
2024-11-21 20:39:01 -0300

    added the above media exercise

---
+++ JavaExercises/src/com/dir/exercises/AboveMedia.java
@@ -0,0 +1,33 @@
+package com.dir.exercises;
+
+import java.util.Scanner;
+
+public class AboveMedia {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] strNums = sc.nextLine().split(" ");
+
+        int total = 0;
+        for (String num : strNums) {
+            total += Integer.parseInt(num);
+        }
+
+        float media = (float) total / strNums.length;
+        String result = "";
+        boolean first = true;
+
+        for (int i = 0; i < strNums.length; i++) {
+            if (Integer.parseInt(strNums[i]) > media) {
+                if (first) {
+                    result += strNums[i];
+                    first = false;
+                } else {
+                    result += " " + strNums[i];
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+}
